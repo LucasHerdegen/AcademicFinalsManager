02fea075dc3deb49bce10e5a658ced7d76307ab2
LucasHerdegen <lherdegen@frba.utn.edu.ar>
2026-01-26 23:39:16 -0300

    subject service tests

--- AcademicFinals.Tests/Services/SubjectServiceTests.cs
+++ AcademicFinals.Tests/Services/SubjectServiceTests.cs
@@ -71,5 +71,166 @@ namespace AcademicFinals.Tests.Services
             _mockRepo.Verify(repo => repo.GetById(id), Times.Once);
             _mockMapper.Verify(mapper => mapper.Map<SubjectDto>(subject), Times.Once);
         }
+
+        [Fact]
+        public async Task CreateSubject_DeberiaRetornarNull_SiLaMateriaYaExiste()
+        {
+            // arrange
+            var dto = new SubjectPostDto
+            {
+                Name = "Matemáticas",
+            };
+
+            _mockRepo.Setup(repo => repo.Exists(s => s.Name.ToUpper() == dto.Name!.ToUpper()))
+                .ReturnsAsync(true);
+
+            // act
+            var result = await _service.CreateSubject(dto);
+
+            // assert
+            result.Should().BeNull();
+            _mockRepo.Verify(repo => repo.Exists(s => s.Name.ToUpper() == dto.Name!.ToUpper()), Times.Once);
+        }
+
+        [Fact]
+        public async Task CreateSubject_DeberiaRetornar_UnaMateria_SiLaMateria_NoExiste()
+        {
+            // arrange
+            int id = 10;
+            string name = "Matemáticas";
+
+            var dto = new SubjectPostDto
+            {
+                Name = name,
+            };
+            var subject = new Subject
+            {
+                Name = name,
+                Id = id
+            };
+            var subjectDto = new SubjectDto
+            {
+                Name = name,
+                Id = id,
+            };
+
+            _mockRepo.Setup(repo => repo.Exists(s => s.Name.ToUpper() == dto.Name!.ToUpper()))
+                .ReturnsAsync(false);
+            _mockMapper.Setup(mapper => mapper.Map<Subject>(dto))
+                .Returns(subject);
+            _mockRepo.Setup(repo => repo.GetById(id))
+                .ReturnsAsync(subject);
+            _mockMapper.Setup(mapper => mapper.Map<SubjectDto>(subject))
+                .Returns(subjectDto);
+
+            // act
+            var result = await _service.CreateSubject(dto);
+
+            // assert
+            result.Should().NotBeNull();
+            result.Should().Be(subjectDto);
+            _mockRepo.Verify(repo => repo.Exists(s => s.Name.ToUpper() == dto.Name!.ToUpper()), Times.Once);
+            _mockMapper.Verify(mapper => mapper.Map<Subject>(dto), Times.Once);
+            _mockRepo.Verify(repo => repo.Create(subject), Times.Once);
+            _mockRepo.Verify(repo => repo.Save(), Times.Once);
+        }
+
+        [Fact]
+        public async Task UpdateSubject_DeberiaRetornarFalse_SiLaMateria_NoExiste()
+        {
+            // arrange
+            int id = 10;
+            string name = "Matemáticas";
+
+            var dto = new SubjectPutDto
+            {
+                Id = id,
+                Name = name,
+            };
+
+            _mockRepo.Setup(repo => repo.GetById(id))
+                .ReturnsAsync((Subject?)null);
+
+            // act
+            var result = await _service.UpdateSubject(dto);
+
+            // assert
+            result.Should().BeFalse();
+            _mockRepo.Verify(repo => repo.GetById(id), Times.Once);
+        }
+
+        [Fact]
+        public async Task UpdateSubject_DeberiaRetornarTrue_SiLaMateriaExiste()
+        {
+            int id = 10;
+            string name = "Matemáticas";
+
+            var dto = new SubjectPutDto
+            {
+                Id = id,
+                Name = name,
+            };
+            var subject = new Subject
+            {
+                Id = id,
+                Name = name,
+            };
+
+            _mockRepo.Setup(repo => repo.GetById(id))
+                .ReturnsAsync(subject);
+
+            // act
+            var result = await _service.UpdateSubject(dto);
+
+            // assert
+            result.Should().BeTrue();
+            _mockRepo.Verify(repo => repo.GetById(id), Times.Once);
+            _mockMapper.Verify(mapper => mapper.Map(dto, subject), Times.Once);
+            _mockRepo.Verify(repo => repo.Update(subject), Times.Once);
+            _mockRepo.Verify(repo => repo.Save(), Times.Once);
+        }
+
+        [Fact]
+        public async Task DeleteSubject_DeberiaRetornarFalse_SiLaMateria_NoExiste()
+        {
+            // arrange
+            int id = 10;
+
+            _mockRepo.Setup(repo => repo.GetById(id))
+                .ReturnsAsync((Subject?)null);
+
+            // act
+            var result = await _service.DeleteSubject(id);
+
+            // assert
+            result.Should().BeFalse();
+            _mockRepo.Verify(repo => repo.GetById(id), Times.Once);
+        }
+
+        [Fact]
+        public async Task DeleteSubject_DeberiaRetornarTrue_SiLaMateriaExiste()
+        {
+            // arrange
+            int id = 10;
+            string name = "Matemáticas";
+
+            var subject = new Subject
+            {
+                Name = name,
+                Id = id,
+            };
+
+            _mockRepo.Setup(repo => repo.GetById(id))
+                .ReturnsAsync(subject);
+
+            // act
+            var result = await _service.DeleteSubject(id);
+
+            // assert
+            result.Should().BeTrue();
+            _mockRepo.Verify(repo => repo.GetById(id), Times.Once);
+            _mockRepo.Verify(repo => repo.Delete(subject), Times.Once);
+            _mockRepo.Verify(repo => repo.Save(), Times.Once);
+        }
     }
 }
